52e2121b1edbe706d74484c8fe6a28c8fbbdfa8f
Sri3195 <n160629@rguktn.ac.in>
2023-04-26 09:28:29 +0530

    project structure changed

--- autoloanapi/src/main/kotlin/com/axis/autoloanapi/service/AutoloanService.kt
+++ autoloanapi/src/main/kotlin/com/axis/autoloanapi/service/AutoloanService.kt
@@ -3,8 +3,9 @@ package com.axis.autoloanapi.service
 import com.axis.autoloanapi.model.Autoloan
 import com.axis.autoloanapi.repository.AutoloanRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
-
+@Service
 class AutoloanService(
     @Autowired
     var autoloanRepository: AutoloanRepository
